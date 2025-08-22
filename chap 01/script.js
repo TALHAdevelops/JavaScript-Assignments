e00e54175d0a0ffaaff1ba19ed9b557ece289fe0
MUHAMMAD TALHA <160895671+TALHAdevelops@users.noreply.github.com>
2025-08-22 17:22:25 +0500

    chap1

---
+++ chap 01/script.js
@@ -0,0 +1,27 @@
+// Solution No 1
+
+alert("Welcome Visitor!");
+
+// Solution No 2
+
+alert("Error! Please enter a valid password.");
+
+// Solution No 3
+
+alert("Welcome to JS Land...\nHappy Coding!");
+
+// Solution No 4
+
+alert("Welcome to JS Land...");
+alert("Happy Coding!");
+
+// Solution No 5
+
+console.log("Hello... I can run JS through my web browser's console");
+
+// Solution No 6
+// I used it in my aussie webpage "https://aussie-talha.vercel.app/"
+
+
+//Solution No 7
+// Check placement of script.js in the HTML file
