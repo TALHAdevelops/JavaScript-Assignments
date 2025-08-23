a8d9af6893817f0f92acc3abb2b9a4cbd03da671
MUHAMMAD TALHA <160895671+TALHAdevelops@users.noreply.github.com>
2025-08-23 15:44:08 +0500

    chap 02

---
+++ chap 02/script.js
@@ -0,0 +1,50 @@
+// Solution No 1
+
+var username ;
+
+// Solution No 2
+
+var myName = "Muhammad Talha";
+
+// Solution No 3
+
+var message = "Hello World";
+alert(message);
+
+// Solution No 4
+
+var studentName = "John Doe";
+var age = "15 years old";
+var course = "Certified Mobile Application Development";
+alert(studentName);
+alert(age);
+alert(course);
+
+// Solution No 5
+
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizza);
+
+// Solution No 6
+
+var email = 'example@example.com';
+alert("My email address is " + email);
+
+
+// Solution No 7
+
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+
+// Solution No 8
+
+var HTML = "Yah! I can write HTML content through JavaScript";
+document.write(HTML);
+
+// Solution No 9
+
+var pattern = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(pattern);
+document.write(pattern);
+
+
