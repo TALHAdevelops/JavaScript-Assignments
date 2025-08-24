edfb1ea555617d9bf3d121db8515833dc7e42017
MUHAMMAD TALHA <160895671+TALHAdevelops@users.noreply.github.com>
2025-08-24 10:06:33 +0500

    chap 03

---
+++ chap 03/script.js
@@ -0,0 +1,24 @@
+//Question No 1
+
+var age  = "20 years old";
+alert("I am " + age);
+
+//Question No 2
+
+var visitCount = 14;
+alert("You have visited this site " + visitCount + " times");
+
+
+//Question No 3
+
+var birthYear = 2006;
+document.write("My birth year is " + birthYear + "<br>Data type of my declared variable is number");
+
+//Question No 4
+
+var visitorName = "John Doe";
+var productTitle = "T-shirt(s)";
+var quantity = 5;
+
+
+document.write(visitorName + " ordered " + quantity + " " + productTitle + " on XYZ Clothing Store");   
