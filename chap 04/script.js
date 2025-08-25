79ffa062b55d27b12a08874190d40980c1b4af9e
MUHAMMAD TALHA <160895671+TALHAdevelops@users.noreply.github.com>
2025-08-25 06:59:38 +0500

    chap 04

---
+++ chap 04/script.js
@@ -0,0 +1,33 @@
+//Question No 1
+
+var username;
+var userage;
+var useremail;
+
+//Question No 2
+
+//Legal names
+
+var myName;
+var $price;
+var _age;
+var firstName;
+var lastName;
+
+//Illegal names
+
+// var 1stName; // Cannot start with a number
+// var first-name; // Hyphen is not allowed
+// var my name; // Spaces are not allowed
+// var @username; // Special characters are not allowed
+// var var; // 'var' is a reserved keyword
+
+
+//Question No 3
+
+document.write("<h1>Rules for naming JS variables</h1>");
+document.write("<p>Variable names can only contain numbers, $, and _. For example $my_1stVariable.</p>");
+document.write("<p>Variable names must start with a letter, underscore, or dollar sign.For example $name, _name or name.</p>");
+document.write("<p>Variable names are case-sensitive.</p>");
+document.write("<p>Variable names should not be a JS keyword.</p>");
+
